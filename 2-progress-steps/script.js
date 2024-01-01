6b3259f3faf15ab7a5cf9466e5951b286ca05d5b
Breno Macedo Ernani de Sá <brenomacedo404@gmail.com>
2024-01-01 11:27:40 -0300

    Progress Steps

---
+++ 2-progress-steps/script.js
@@ -0,0 +1,60 @@
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
+
+let currentActive = 1;
+
+prev.addEventListener("click", () => {
+  if (currentActive > 1) {
+    currentActive--;
+  } else {
+    currentActive = 1;
+  }
+
+  update();
+});
+
+next.addEventListener("click", () => {
+  if (currentActive < circles.length) {
+    currentActive++;
+  } else {
+    currentActive = circles.length;
+  }
+
+  update();
+});
+
+circles.forEach((circle, index) => {
+  circle.addEventListener("click", () => {
+    currentActive = index + 1;
+
+    update();
+  });
+});
+
+function update() {
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  const actives = document.querySelectorAll(".active");
+
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  if (currentActive == 1) {
+    prev.disabled = true;
+    next.disabled = false;
+  } else if (currentActive == circles.length) {
+    prev.disabled = false;
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+}
